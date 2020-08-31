d3801c454cce5cdeee370e8c58ae6641dfb17522
Ely Kahn <ely.kahn@gmail.com>
2020-08-31 18:35:03 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
     {
         resolve: 'gatsby-source-prismic-graphql',
         options: {
-            repositoryName: 'elydotio', // (REQUIRED, replace with your own)
+            repositoryName: 'prismicelyio', // (REQUIRED, replace with your own)
             linkResolver: () => post => `/${post.uid}`,
         }
     },
